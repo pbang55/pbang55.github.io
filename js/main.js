185391291147682d3916c6a92b88ac6e7a75807d
Peter Bang <pbang@college.harvard.edu>
2014-08-23 09:00:50 -0700

    DFS and game API set up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+function Sudoku(state) {
+	this.state = state;
+	this.getSuccessors = function() {
+		return [new Sudoku(this.state + 1)];
+	}
+	this.isFinalState = function() {
+		return this.state == 10;
+	}
+}
 
+function DFS(problem) {
+	fringe = [problem]
+	while (fringe.length){
+		debugger
+		state = fringe.pop();
+		if (state.isFinalState())
+			return state;
+		else {
+			successors = state.getSuccessors();
+			_.extend(fringe, successors);
+		}
+	}
+
+	throw("No solution");
+}
